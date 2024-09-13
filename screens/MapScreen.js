0c467ec432e9cb3f271860e7e75113f49e92ed53
wilsonriosv <wilsonriosv@gmail.com>
2024-09-13 13:51:09 -0500

    # Changes to be committed: #	modified:   .gitignore #	modified:   app.json #	modified:   package.json #	modified:   screens/MapScreen.js #	modified:   yarn.lock

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+
+const MapScreen = () => {
+  const [markers, setMarkers] = useState([]);
+
+  const addMarker = (event) => {
+    const newMarker = {
+      coordinate: event.nativeEvent.coordinate,
+      key: Math.random().toString(),
+    };
+    setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
+  };
+
+  const removeMarker = (key) => {
+    setMarkers((prevMarkers) => prevMarkers.filter(marker => marker.key !== key));
+  };
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -34.6037,
+          longitude: -58.3816,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        onPress={addMarker}
+      >
+        {markers.map(marker => (
+          <Marker
+            key={marker.key}
+            coordinate={marker.coordinate}
+            onPress={() => removeMarker(marker.key)}
+          />
+        ))}
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default MapScreen;
+
+
+/* import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const MapScreen = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -34.6037, // Cambia a la latitud deseada
+          longitude: -58.3816, // Cambia a la longitud deseada
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+export default MapScreen; */
+
+
+/* import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const MapScreen = () => {
   return (
     <View style={styles.container}>
       <Text>Mapa</Text>
-      {/* Aquí puedes integrar Google Maps */}
+    // Aquí puedes integrar Google Maps 
     </View>
   );
 };
@@ -19,3 +108,4 @@ const styles = StyleSheet.create({
 });
 
 export default MapScreen;
+ */
